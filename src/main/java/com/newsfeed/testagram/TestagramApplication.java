63246fdedb07d58f1f2665010b2960c0e23cc7bd
NewJKH <newsun0805@gmail.com>
2025-05-27 12:15:09 +0900

    first commit

---
+++ src/main/java/com/newsfeed/testagram/TestagramApplication.java
@@ -0,0 +1,13 @@
+package com.newsfeed.testagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestagramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/newsfeed/testagram/TestagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsfeed.testagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
