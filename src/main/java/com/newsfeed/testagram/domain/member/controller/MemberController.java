09dddbc3d54191519e753d7016ab94be4ef0463a
NewJKH <newsun0805@gmail.com>
2025-05-29 18:39:42 +0900

    feat: "이메일, 비밀번호 검증 적용"

--- src/main/java/com/newsfeed/testagram/domain/member/controller/MemberController.java
+++ src/main/java/com/newsfeed/testagram/domain/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.newsfeed.testagram.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> save(@RequestBody MemberSignUpRequest request) {
+    public ResponseEntity<?> save(@Validated @RequestBody MemberSignUpRequest request) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(memberService.signup(request.getEmail(),request.getPassword(),request.getMemberName()));
