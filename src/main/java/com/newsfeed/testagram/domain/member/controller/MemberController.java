da7f4f576120e00e85652c7ca28fc238fc5b2e78
NewJKH <newsun0805@gmail.com>
2025-05-29 20:19:58 +0900

    pref: "@Validated -> @Valid 수정"

--- src/main/java/com/newsfeed/testagram/domain/member/controller/MemberController.java
+++ src/main/java/com/newsfeed/testagram/domain/member/controller/MemberController.java
@@ -2,10 +2,10 @@ package com.newsfeed.testagram.domain.member.controller;
 
 import com.newsfeed.testagram.domain.member.dto.MemberSignUpRequest;
 import com.newsfeed.testagram.domain.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +19,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> save(@Validated @RequestBody MemberSignUpRequest request) {
+    public ResponseEntity<?> save(@Valid @RequestBody MemberSignUpRequest request) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(memberService.signup(request.getEmail(),request.getPassword(),request.getMemberName()));
