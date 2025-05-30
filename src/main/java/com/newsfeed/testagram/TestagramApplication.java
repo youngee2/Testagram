915d9e34717dfcebad291ce33ac65a106a4747f7
ahyoung <jr3537@naver.com>
2025-05-30 09:48:00 +0900

    fix: JPA Auditing 누락 설정 추가

--- src/main/java/com/newsfeed/testagram/TestagramApplication.java
+++ src/main/java/com/newsfeed/testagram/TestagramApplication.java
@@ -2,7 +2,9 @@ package com.newsfeed.testagram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TestagramApplication {
 
--- src/main/java/com/newsfeed/testagram/member/entity/Member.java
+++ src/main/java/com/newsfeed/testagram/member/entity/Member.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Table(name="member")
 @AllArgsConstructor
