13597dc1ee4cd7eab2972ce4cba9fc6a033b1534
NewJKH <newsun0805@gmail.com>
2025-05-30 20:11:57 +0900

    test: "아이디 정상등록 테스트(완)"

--- src/main/java/com/newsfeed/testagram/domain/login/controller/LoginController.java
+++ src/main/java/com/newsfeed/testagram/domain/login/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.newsfeed.testagram.domain.login.controller;
 
 import com.newsfeed.testagram.common.util.JwtUtil;
 import com.newsfeed.testagram.domain.login.dto.LoginRequest;
+import com.newsfeed.testagram.domain.login.dto.LoginResponse;
 import com.newsfeed.testagram.domain.login.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,7 +20,7 @@ public class LoginController {
     private final JwtUtil jwtUtil;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
         return ResponseEntity.
                 status(HttpStatus.OK)
                 .body(loginService.login(request.getEmail(),request.getPassword()));
@@ -28,7 +29,7 @@ public class LoginController {
     @GetMapping("/member/me")
     public ResponseEntity<?> getMyInfo(@RequestHeader("Authorization") String authHeader) {
         String token = authHeader.replace(BEARER_PREFIX, "");
-        return ResponseEntity.ok("내 이메일 " + jwtUtil.getEmailFromToken(token));
+        return ResponseEntity.ok("내 이메일 " + jwtUtil.getEmailFromToken(token)+"내 아이디:"+jwtUtil.getMemberIdFormToken(token));
     }
 
 }
