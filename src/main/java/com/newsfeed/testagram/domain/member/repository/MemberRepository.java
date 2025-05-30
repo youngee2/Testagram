3e1cfd3a791f87c6401c6528611126645c347c86
NewJKH <newsun0805@gmail.com>
2025-05-30 12:49:07 +0900

    pref: "ResponseHttpStatusException -> 스텀 예외(EmailNotFoundException 으로 수정 "

--- src/main/java/com/newsfeed/testagram/domain/member/repository/MemberRepository.java
+++ src/main/java/com/newsfeed/testagram/domain/member/repository/MemberRepository.java
@@ -1,9 +1,8 @@
 package com.newsfeed.testagram.domain.member.repository;
 
+import com.newsfeed.testagram.common.exception.member.EmailNotFoundException;
 import com.newsfeed.testagram.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -11,7 +10,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
     default Member findByEmailOrThrow(String email){
         return this.findByEmail(email)
-                .orElseThrow(()->new ResponseStatusException(HttpStatus.BAD_REQUEST," 없는 이메일"));
+                .orElseThrow(EmailNotFoundException::new);
     }
     Optional<Member> findByNickname(String nickname);
 }
